dfabfd74f146b9d0f41ba63803bb9a21ccfbc666
elflacoseba <sebaserri@gmail.com>
2024-05-05 16:55:11 -0300

    Creando la página Markers

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 13;
+  public map?: Map;
+  public lngLat: LngLat = new LngLat(-65.3049935, -26.832835);
+
+ngAfterViewInit(): void {
+
+  if ( !this.divMap ) throw 'El elemento divMap no esta definido';
+
+  this.map = new Map({
+    container: this.divMap.nativeElement, // element container
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: this.lngLat, // starting position [lng, lat]
+    zoom: this.zoom, // starting zoom
+  });
+
+  const marker = new Marker({
+    color: 'red',
+  })
+  .setLngLat( this.lngLat)
+  .addTo(this.map);
+
+}
 
 }
