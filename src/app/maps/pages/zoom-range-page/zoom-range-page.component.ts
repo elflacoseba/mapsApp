3033d0c0821dc8d7529f895f6636d2e6bf0f87bf
elflacoseba <sebaserri@gmail.com>
2024-05-05 16:14:24 -0300

    Creo el mapa en la página zoom-range con su control para el zoom.

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit{
 
+    @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw 'El elemento divMap no esta definido';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // element container
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-65.3049935, -26.832835], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
