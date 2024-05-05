d4d82667cdf7a57c0e7c3d87d94124b72348adf5
elflacoseba <sebaserri@gmail.com>
2024-05-05 16:33:28 -0300

    Programando el zoom del mapa.

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,15 +9,52 @@ export class ZoomRangePageComponent implements AfterViewInit{
 
     @ViewChild('map') divMap?: ElementRef;
 
+    public zoom: number = 10;
+    public map?: Map;
+
   ngAfterViewInit(): void {
 
     if ( !this.divMap ) throw 'El elemento divMap no esta definido';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // element container
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-65.3049935, -26.832835], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
   }
+
+  mapListeners() {
+
+    if ( !this.map ) throw 'El mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+
+      if (this.map!.getZoom() < 18) return;
+
+      this.map!.zoomTo(18);
+    });
+
+  }
+
+  zoomIn(): void {
+    this.map?.zoomIn();
+  }
+
+  zoomOut(): void {
+    this.map?.zoomOut();
+  }
+
+  zoomChanges(value: string): void {
+    this.zoom = Number(value);
+
+    this.map?.zoomTo( this.zoom );
+  }
+
 }
