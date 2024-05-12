6c68e08579dd4a2f5d159f8036f1f1fdf6430ea1
elflacoseba <sebaserri@gmail.com>
2024-05-12 20:42:14 -0300

    Creo los ítems del marcador con su color y los elimino con doble click.

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,6 +1,11 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { LngLat, Map, Marker } from 'mapbox-gl';
 
+interface MarkerColor {
+  color: string;
+  marker?: Marker;
+}
+
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
@@ -9,26 +14,21 @@ export class MarkersPageComponent implements AfterViewInit {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public markers: MarkerColor[] = [];
   public zoom: number = 13;
   public map?: Map;
   public lngLat: LngLat = new LngLat(-65.3049935, -26.832835);
 
 ngAfterViewInit(): void {
 
-  if ( !this.divMap ) throw 'El elemento divMap no esta definido';
+    if ( !this.divMap ) throw 'El elemento divMap no esta definido';
 
-  this.map = new Map({
-    container: this.divMap.nativeElement, // element container
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: this.lngLat, // starting position [lng, lat]
-    zoom: this.zoom, // starting zoom
-  });
-
-  const marker = new Marker({
-    color: 'red',
-  })
-  .setLngLat( this.lngLat)
-  .addTo(this.map);
+    this.map = new Map({
+      container: this.divMap.nativeElement, // element container
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
 
   }
 
@@ -51,6 +51,14 @@ ngAfterViewInit(): void {
       draggable: true
     }).setLngLat( lngLat )
     .addTo(this.map);
+
+    this.markers.push( { color, marker } );
+  }
+
+  deleteMarker( i: number ) {
+
+    this.markers[i].marker?.remove();
+    this.markers.splice(i, 1);
 
   }
 
