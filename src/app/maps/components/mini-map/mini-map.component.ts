bf74ab97aedd77dc25df502ca37da3bfc330ce5f
elflacoseba <sebaserri@gmail.com>
2024-05-13 15:31:42 -0300

    Creo los minimapas en la página de propiedades.

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker} from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
+
 export class MiniMapComponent {
+
+  @ViewChild('map') divMap?: ElementRef;
+  @Input() lngLat?: [number, number];
+
+  ngAfterViewInit() {
+
+    if (!this.divMap) throw 'El elemento divMap no esta definido';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // element container
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat || [0, 0], // starting position [lng, lat] with default value
+      zoom: 13, // starting zoom
+      interactive: false // disable zoom and move
+    });
+
+    if (this.lngLat) {
+      new Marker()
+        .setLngLat(this.lngLat)
+        .addTo(map);
+    }
+
+  }
+
 
 }
